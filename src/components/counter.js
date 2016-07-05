ec240af4324a88da8e1759fe97480e8134eb0c0d
黄建峰 <645381995@qq.com>
2016-07-05 18:24:37 +0800

    1

--- src/components/counter.js
+++ src/components/counter.js
@@ -6,6 +6,8 @@ let yeomanImage = require('../images/yeoman.png');
 class Counter extends Component {
     render() {
         const {increment, incrementIfOdd, incrementAsync, decrement, counter} = this.props;
+        const txt='异步';
+        const txt2=`${txt}加`;
         return (
             <div className="index">
                 <img src={yeomanImage} alt="Yeoman Generator" />
@@ -19,7 +21,7 @@ class Counter extends Component {
                         {' '}
                         <button onClick={incrementIfOdd}>如果是奇数就加</button>
                         {' '}
-                        <button onClick={() => incrementAsync() }>异步加</button>
+                        <button onClick={() => incrementAsync() }>{txt2}</button>
                     </div></div>
 
             </div>
