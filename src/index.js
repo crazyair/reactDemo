546ea6d2b0d761b5149d87c30649dcd3af1b1293
黄建峰 <645381995@qq.com>
2016-07-09 12:00:12 +0800

    a

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './stores';
 import App from './containers/App';
-
+// import {Button}  from 'antd';
+import Dialog from 'rc-dialog';
 const store = configureStore();
 
 render(
